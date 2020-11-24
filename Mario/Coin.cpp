2dd005c561f269875193d4a5055cf19beabb5e8c
WayciQ <hoangquang0511@gmail.com>
2020-11-24 17:48:33 +0700

    Revert "Revert "item""
    This reverts commit 54652a4d69f0b92d3a3a4523c5524a1364540661.

--- Mario/Coin.cpp
+++ Mario/Coin.cpp
@@ -1,1 +1,32 @@
 #include "Coin.h"
+Coin::Coin(float x, float y, bool isStatic) : Item(x, y)
+{
+	this->isStatic = isStatic;
+	type = COIN;
+	widthBBox = heightBBox = 16;
+	CurAnimation = animationsSets->Get(COIN)->Get(BLOCK_STATIC);
+	vy = -0.2f;
+	time = GetTickCount();
+
+}
+void Coin::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects) {
+	GameObject::Update(dt);
+	vy += WORLD_GRAVITY * dt;
+	y += dy;
+	if (isStatic) {
+		vy = 0;
+	}
+	else {
+		if (GetTickCount() - time >= 400)
+		{
+			isDead = true;
+		}
+	}
+
+	if (isDead)
+	{
+		vy = 0;
+		canDel = true;
+	}
+
+}
--- Mario/Coin.h
+++ Mario/Coin.h
@@ -2,20 +2,11 @@
 #include "Item.h"
 class Coin : public Item
 {
+	bool isStatic;
+	int time;
 public:
-	Coin(float x, float y,bool isStatic = true) : Item(x, y)
-	{
-		
-		type = COIN;
-		widthBBox = heightBBox = 16;
-		CurAnimation = animationsSets->Get(COIN)->Get(BLOCK_STATIC);
-	}
+	Coin(float x, float y, bool isStatic = true);
 	~Coin() {};
-	void GetBoundingBox(float& left, float& top, float& right, float& bottom) {
-		left = x;
-		top = y;
-		right = x + widthBBox;
-		bottom = y + heightBBox;
-	};
+	void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects = NULL);
 };
 
